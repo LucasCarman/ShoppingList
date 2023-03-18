aacb1d7c0a985e27c8a7486592303603761f3a83
Lucas Carman <lcarm014@fiu.edu>
2023-03-18 03:12:39 -0400

    removed comments

--- main.py
+++ main.py
@@ -21,20 +21,6 @@ def itemEnter(item, quantity, unit, comment, card):
     except:
         ui.notify("Entered items must be unique")
 enterCard = ui.card()
-#with ui.row():
-#    with enterCard:
-#        label = ui.label('Shopping List')
-#        with ui.column():
-#            with ui.row():
-#                textInput = ui.input(label='Text', placeholder='item name',
-#                     validation={'Input too long': lambda value: len(value) < 25})
-#                quantityInput = ui.number(label="Quantity",format='%d', value=1)
-#                unitInput = ui.input(label='Unit', placeholder='unit')
-#                commentInput = ui.input(label='Insert any comments', placeholder='comments')
-#                ui.button("Enter Item", on_click=lambda e: itemEnter(textInput, quantityInput, unitInput, commentInput, enterCard))
-#    with ui.column():
-#        picker = ui.color_picker(on_pick=lambda e: ui.colors(primary =  e.color))
-#        button = ui.button(text = 'Set Color Theme', on_click=picker.open)
 
 def editData():
     pass
