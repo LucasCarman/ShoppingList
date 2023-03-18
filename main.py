092a5c286318138d1cbb1cd3763dfab8a034b69e
Lucas Carman <lcarm014@fiu.edu>
2023-03-18 03:11:15 -0400

    clear entercard

--- main.py
+++ main.py
@@ -5,32 +5,36 @@ con = sqlite3.connect("ShoppingList.db")
 cur = con.cursor()
 
 get_id = ui.label()
-def itemEnter(item, quantity, unit, comment):
+def itemEnter(item, quantity, unit, comment, card):
     try:
         if item=='':
             ui.notify("Item cannot be empty")
         else:
-            cur.execute("INSERT INTO items(itemName, quantity, unit, comment) VALUES(?, ?, ?, ?)", (item, quantity,  unit, comment))
+            cur.execute("INSERT INTO items(itemName, quantity, unit, comment) VALUES(?, ?, ?, ?)", (item.value, quantity.value,  unit.value, comment.value))
             con.commit()
-            ui.notify(f'Successfully added {int(quantity)} {item}')
+            ui.notify(f'Successfully added {int(quantity.value)} {item.value}')
             list_alldata.clear()
+            enterCard.clear()
             getData()
+            
+
     except:
         ui.notify("Entered items must be unique")
-with ui.row():
-    with ui.card():
-        label = ui.label('Shopping List')
-        with ui.column():
-            with ui.row():
-                textInput = ui.input(label='Text', placeholder='item name',
-                     validation={'Input too long': lambda value: len(value) < 25})
-                quantityInput = ui.number(label="Quantity",format='%d', value=1)
-                unitInput = ui.input(label='Unit', placeholder='unit')
-                commentInput = ui.input(label='Insert any comments', placeholder='comments')
-                ui.button("Enter Item", on_click=lambda e: itemEnter(textInput.value, quantityInput.value, unitInput.value, commentInput.value))
-    with ui.column():
-        picker = ui.color_picker(on_pick=lambda e: ui.colors(primary =  e.color))
-        button = ui.button(text = 'Set Color Theme', on_click=picker.open)
+enterCard = ui.card()
+#with ui.row():
+#    with enterCard:
+#        label = ui.label('Shopping List')
+#        with ui.column():
+#            with ui.row():
+#                textInput = ui.input(label='Text', placeholder='item name',
+#                     validation={'Input too long': lambda value: len(value) < 25})
+#                quantityInput = ui.number(label="Quantity",format='%d', value=1)
+#                unitInput = ui.input(label='Unit', placeholder='unit')
+#                commentInput = ui.input(label='Insert any comments', placeholder='comments')
+#                ui.button("Enter Item", on_click=lambda e: itemEnter(textInput, quantityInput, unitInput, commentInput, enterCard))
+#    with ui.column():
+#        picker = ui.color_picker(on_pick=lambda e: ui.colors(primary =  e.color))
+#        button = ui.button(text = 'Set Color Theme', on_click=picker.open)
 
 def editData():
     pass
@@ -39,9 +43,24 @@ def deleteData(data):
     cur.execute('delete from items where itemName = ?', [data.text])
     con.commit()
     list_alldata.clear()
+    enterCard.clear()
     getData()
 list_alldata = ui.column()
 def getData():
+        with ui.row():
+            with enterCard:
+                label = ui.label('Shopping List')
+                with ui.column():
+                    with ui.row():
+                        textInput = ui.input(label='Text', placeholder='item name',
+                             validation={'Input too long': lambda value: len(value) < 25})
+                        quantityInput = ui.number(label="Quantity",format='%d', value=1)
+                        unitInput = ui.input(label='Unit', placeholder='unit')
+                        commentInput = ui.input(label='Insert any comments', placeholder='comments')
+                        ui.button("Enter Item", on_click=lambda e: itemEnter(textInput, quantityInput, unitInput, commentInput, enterCard))
+                with ui.column():
+                    picker = ui.color_picker(on_pick=lambda e: ui.colors(primary =  e.color))
+                    button = ui.button(text = 'Set Color Theme', on_click=picker.open)
         cur.execute('select * from items')
         res = cur.fetchall()
         result = []
